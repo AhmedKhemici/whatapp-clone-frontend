fc2e0f546a21b950c8cc492a2cc9b60da537e569
Ahmed Khemici <ahmed.khemici@yib.io>
2023-10-28 23:52:27 +0100

    added login form

--- src/App.js
+++ src/App.js
@@ -3,49 +3,50 @@ import './App.css';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
-import { socket } from './socket';
+// import { socket } from './socket';
 
 const App =() => {
   const [user, setUser] = useState({});
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+  // const [isConnected, setIsConnected] = useState(socket.connected);
+  // const [fooEvents, setFooEvents] = useState([]);
+  // useEffect(() => {
+  //   function onConnect() {
+  //     setIsConnected(true);
+  //   }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+  //   function onDisconnect() {
+  //     setIsConnected(false);
+  //   }
 
-    function onFooEvent(value) {
-      setFooEvents(previous => [...previous, value]);
-    }
+  //   function onFooEvent(value) {
+  //     setFooEvents(previous => [...previous, value]);
+  //   }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
+  //   socket.on('connect', onConnect);
+  //   socket.on('disconnect', onDisconnect);
+  //   socket.on('foo', onFooEvent);
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
-    };
-  }, []);
+  //   return () => {
+  //     socket.off('connect', onConnect);
+  //     socket.off('disconnect', onDisconnect);
+  //     socket.off('foo', onFooEvent);
+  //   };
+  // }, []);
 
-  const Login = (
+  const LoginForm = (
     <>
       <Login setUser={setUser} />
     </>
   );
+
   const whatsAppBody = (
       <>
         <Sidebar userData={user} />
         <Chat userData={user} /> 
       </>
     );
-  const body = [user._id ? loginForm : whatsAppBody]
-
+  const body = [!user._id ? LoginForm : whatsAppBody]
+  console.log(user._id)
   return (
     <div className="app">
       <div className="app__body">
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import './Login.css';
 
 const Login = ( props) => {
   const loginUser = (event) =>{
-    event.default();
+    // event.preventDefault();
     axios.post('http://127.0.0.1:9000/api/v1/login',{name:event.target.name, password:event.target.password}).then(result => {
       console.log(`Messages Returned`);
       console.log(result);
@@ -13,12 +14,15 @@ const Login = ( props) => {
       console.log(err);
     });
   }
-  return (   
-      <form action="" className="form">
-        <input name="name" type="text" className="form__input" placeholder="Username" />
-        <input name="password" type="password" className="form__input" placeholder="*******" />
-        <button type="submit" onSubmit={() => loginUser} className="form__button"></button>
-      </form>
+  return (
+      <div className="form"> 
+        <WhatsAppIcon />
+        <form action="post" onSubmit={(event) => loginUser(event)}>
+          <input name="name" type="text" className="form__input" placeholder="Username" />
+          <input name="password" type="password" className="form__input" placeholder="*******" />
+          <button type="submit"  className="form__button">login</button>
+        </form>
+      </div>
   )
 }
 
